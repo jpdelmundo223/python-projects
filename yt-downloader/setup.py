ad6f58ae1e99ec5c7776aa731dacad063a762d47
John Paul Del Mundo <john.delmundo@CEGROUP.com>
2022-09-09 08:08:25 +0800

    configured setup.py

---
+++ yt-downloader/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+install_requires = [
+    "pytube"
+]
+
+setup(
+    name="YouTube Downloader",
+    version="1.0",
+    author="John Paul Del Mundo",
+    author_email="jpdelmundo223@gmail.com",
+    description="A Python library for downloading media streams from YouTube.",
+    keywords="youtube downloader python pytube",
+    requires=install_requires,
+    url="https://github.com/jpdelmundo223/python-projects/tree/master/yt-downloader"
+)
